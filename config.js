fa50b8cf522b2993e2cf77e8ffb6a3140fd4b018
Kelvin Unix <162993206+Kpunix@users.noreply.github.com>
2024-06-18 23:16:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '+255734980103'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '+255678925070'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'lazaromtaju@gmail.com'
-global.github = 'https://github.com/Lazack28/Lazack-md'
+global.email = 'kelvinunixtechnologies@gmail.com'
+global.github = 'https://github.com/Kpunix/Kp-unix-MD'
 global.location = 'Dodoma Tanzania'
-global.gurl = 'https://instagram.com/Lazack_28/' // add your username
-global.sudo = process.env.SUDO || "255734980103"
-global.devs = '+255734980103'
-global.website = 'https://github.com/Lazack28/Lazack-md' //wa.me/+255734980103
+global.gurl = 'https://instagram.com/kelvin_unix/' // add your username
+global.sudo = process.env.SUDO || "255678925070"
+global.devs = '+255678925070'
+global.website = 'https://github.com/Kpunix/Kp-unix-MD' //wa.me/+255678925070
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/2cbb7abbf988bf9e7a088.jpg'
 module.exports = {
   sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
-  author:  process.env.PACK_AUTHER ||  'LAZACK BOT',
-  packname:  process.env.PACK_NAME || 'CREDIT TO LAZACK',
+  author:  process.env.PACK_AUTHER ||  'KP UNIX BOT',
+  packname:  process.env.PACK_NAME || 'CREDIT TO KELVIN UNIX',
   
-  botname:   process.env.BOT_NAME === undefined ? "LAZACK-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿' : process.env.OWNER_NAME,  
+  botname:   process.env.BOT_NAME === undefined ? "KP-UNIX-MD" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𝑲𝒑-𝒖𝒏𝒊𝒙 𝑴𝑫' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
@@ -37,7 +37,7 @@ module.exports = {
   BRANCH: 'main', 
   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
   autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
-  caption :process.env.CAPTION || "\t*•credit to Lazack28•* ",   //*『sᴜʙsᴄʀɪʙᴇ • ʙʟᴀᴅᴇ ᴛᴇᴄʜ』*\n youtube.com/@blade444"),	
+  caption :process.env.CAPTION || "\t*•credit to Kelvin Unix•* ",   //*『sᴜʙsᴄʀɪʙᴇ • ʙʟᴀᴅᴇ ᴛᴇᴄʜ』*\n youtube.com/@blade444"),	
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
   HEROKU: {
@@ -46,7 +46,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'LAZACK',
+  LANG: process.env.THEME|| 'KELVIN UNIX',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
